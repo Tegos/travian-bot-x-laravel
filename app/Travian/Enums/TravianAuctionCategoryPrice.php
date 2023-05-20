bd7371701643358b3d4e6656cde8ed5d95b8ee4f
Ivan Mykhavko <tegosiv@gmail.com>
2023-05-20 19:09:22 +0300

    update price auction

--- app/Travian/Enums/TravianAuctionCategoryPrice.php
+++ app/Travian/Enums/TravianAuctionCategoryPrice.php
@@ -12,14 +12,14 @@ final class TravianAuctionCategoryPrice
     const RIGHT_HAND = 105;
     const SHOES = 122;
     const HORSE = 222;
-    const BANDAGE25 = 4;
-    const BANDAGE33 = 7;
-    const CAGE = 7;
-    const SCROLL = 10;
-    const OINTMENT = 7;
-    const BUCKET_OF_WATER = 555;
-    const BOOK_OF_WISDOM = 222;
-    const LAW_TABLETS = 15;
+    const BANDAGE25 = 3;
+    const BANDAGE33 = 5;
+    const CAGE = 5;
+    const SCROLL = 5;
+    const OINTMENT = 3;
+    const BUCKET_OF_WATER = 222;
+    const BOOK_OF_WISDOM = 111;
+    const LAW_TABLETS = 9;
     const ART_WORK = 555;
 
     public static function getPrice(string $itemCategory)
